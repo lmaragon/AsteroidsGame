160126d091591586ad3872744226907ee29ef66f
unknown <luaragon@697-334-Rohan.sf.k12.internal>
2014-11-07 11:00:37 -0800

    Added first naem to index.html

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip battleStarGalactica = new SpaceShip();
 Star [] galaxy;
 
@@ -34,11 +50,11 @@ public void keyPressed()
   }
   if (keyCode == UP)
   {
-    battleStarGalactica.accelerate(.2);
+    battleStarGalactica.accelerate(.2f);
   }
   if (keyCode == DOWN)
   {
-    battleStarGalactica.accelerate(-.2);
+    battleStarGalactica.accelerate(-.2f);
   }
   if (key == ' ')
   {
@@ -172,3 +188,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
